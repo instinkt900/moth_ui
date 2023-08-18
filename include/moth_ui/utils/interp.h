617153cd6bed062b16ddcc2b473db46967a0534e
Matthew Cotton <instinkt@gmail.com>
2023-08-18 10:02:47 +1000

    Sorting m_pi issues on linux.

--- include/moth_ui/utils/interp.h
+++ include/moth_ui/utils/interp.h
@@ -3,6 +3,9 @@
 #undef M_PI
 #define _USE_MATH_DEFINES
 #include <math.h>
+#ifndef M_PI
+#define M_PI (3.14159265358979323846)
+#endif
 
 namespace moth_ui {
     // Smoothing functions from http://easings.net/ (With visual examples)
