b16e5b60027cc4a85a12174f175ee98de58fb4a9
Matthew Cotton <instinkt@gmail.com>
2023-08-26 18:39:32 +1000

    Adding harfbuzz usage. Rebuilt font rendering and word wrapping.

--- backends/include/vulkan/vulkan_font.h
+++ backends/include/vulkan/vulkan_font.h
@@ -4,21 +4,31 @@
 #include "vulkan_buffer.h"
 #include "vulkan_graphics.h"
 
+#include "hb.h"
+
 namespace backend::vulkan {
     class Font : public moth_ui::IFont {
     public:
         static std::shared_ptr<Font> Load(char const* path, int size, Context& context, Graphics& graphics);
         virtual ~Font();
 
-        VkDescriptorSet GetVKDescriptorSet() const { return m_vkDescriptorSet; }
-
         int32_t GetLineHeight() const { return m_lineHeight; }
+        int32_t GetAscent() const { return m_ascent; }
 
         int GetGlyphIndex(int charCode) const;
-        moth_ui::IntVec2 GetGlyphSize(int charCode) const;
-        int32_t GetGlyphWidth(int charCode) const;
+
+        moth_ui::IntVec2 const& GetGlyphSize(int glyphIndex) const;
+        moth_ui::IntVec2 const& GetGlyphBearing(int glyphIndex) const;
+
         int32_t GetStringWidth(std::string_view const& str) const;
         int32_t GetColumnHeight(std::string const& str, int32_t width) const;
+
+        struct ShapedInfo {
+            uint32_t glyphIndex;
+            moth_ui::IntVec2 advance;
+            moth_ui::IntVec2 offset;
+        };
+        std::vector<ShapedInfo> ShapeString(std::string_view const& str) const;
 
         struct LineDesc {
             int32_t lineWidth;
@@ -26,22 +36,41 @@ namespace backend::vulkan {
         };
         std::vector<LineDesc> WrapString(std::string const& str, int32_t width) const;
 
+        VkDescriptorSet GetVKDescriptorSet() const { return m_vkDescriptorSet; }
+
     private:
         Font(FT_Face face, int size, Context& context, Graphics& graphics);
 
-        struct GlyphInfo {
+        // the freetype face object
+        FT_Face m_ftFace;
+
+        // harfbuzz stuff. this should probably be wrapped
+        hb_font_t* m_hbFont = nullptr;
+        mutable hb_buffer_t* m_hbBuffer = nullptr;
+
+        // texture containing all glyphs
+        std::unique_ptr<Image> m_glyphAtlas;
+
+        // for mapping char code to glyph index
+        std::map<int, int> m_charCodeToIndex;
+
+        // global font measurements
+        int32_t m_lineHeight;
+        int32_t m_ascent;
+
+        struct ShaderInfo {
             moth_ui::IntVec2 Size;
-            moth_ui::IntVec2 Advance;
+            moth_ui::IntVec2 Unused;
             moth_ui::FloatVec2 UV0;
             moth_ui::FloatVec2 UV1;
         };
 
-        int32_t m_lineHeight;
-        std::map<int, int> m_charCodeToIndex;
-        std::vector<GlyphInfo> m_glyphInfo;
-        std::unique_ptr<Image> m_glyphAtlas;
+        std::vector<moth_ui::IntVec2> m_glyphBearings;  // glyph bearing values
+        std::vector<ShaderInfo> m_shaderInfos;          // glyph info specifically for the shader
 
         VkDescriptorSet m_vkDescriptorSet;
-        std::unique_ptr<Buffer> m_storageBuffer;
+        std::unique_ptr<Buffer> m_glyphInfosBuffer;     // the buffer that stores the glyph infos
+
+        std::map<int, uint32_t> m_codepointToIndex;
     };
 }
