cc581842140196fbf6fd19c284afa26aebb2a762
Matthew Cotton <instinkt@gmail.com>
2023-10-11 17:03:26 +1100

    Adding a script to build required libs for the visual studio solution.

---
+++ project/vs/prereqs.bat
@@ -0,0 +1,36 @@
+@echo off
+
+echo "Setting up Moth UI prerequesites for VS build..."
+
+pushd ..\..\external
+
+rmdir fmt\build /s /q
+mkdir fmt\build
+cd fmt\build
+
+cmake .. && cmake --build . --config Release && cmake --install . --config Release --prefix=installed
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd ..\..
+
+rmdir freetype\build /s /q
+mkdir freetype\build
+cd freetype\build
+cmake .. && cmake --build . --config Release && cmake --install . --config Release --prefix=installed
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd ..\..
+
+rmdir harfbuzz-icu-freetype\build /s /q
+mkdir harfbuzz-icu-freetype\build
+cd harfbuzz-icu-freetype\build
+cmake .. && cmake --build . --config Release && cmake --install . --config Release --prefix=installed
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd ..\..
+
+rmdir spdlog\build /s /q
+mkdir spdlog\build
+cd spdlog\build
+cmake .. && cmake --build . --config Release && cmake --install . --config Release --prefix=installed
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd ..\..
+
+popd
