ca5d20cb0cac469d65db07711dcf63f3bde25c02
Matthew Cotton <instinkt900@hotmail.com>
2023-10-29 15:32:34 +1100

    fixing more conan bullshit.

--- editor/conanfile.py
+++ editor/conanfile.py
@@ -7,7 +7,7 @@ class Editor(ConanFile):
 	generators = "CMakeToolchain", "CMakeDeps"
 
 	def requirements(self):
-		self.requires("sdl/2.28.3")
+		self.requires("sdl/2.28.3", override=True)
 		self.requires("sdl_image/2.0.5")
 		self.requires("sdl_ttf/2.20.2")
 		self.requires("nlohmann_json/3.11.2")
