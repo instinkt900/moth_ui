d6563b40145497587df840a61f5967279faacd27
Matthew Cotton <instinkt@gmail.com>
2025-04-22 18:33:24 +1000

    feat: Brought back example project.
    Brought back the example project and all its assets. I'm mostly
    interested in the assets since it was good for testing. The actual
    example project running is not so important.

---
+++ example/src/example_layer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "moth_ui/layers/layer.h"
+#include "moth_ui/events/event_animation.h"
+#include "events/event.h"
+
+#include <filesystem>
+
+class ExampleLayer : public moth_ui::Layer {
+public:
+    ExampleLayer(std::filesystem::path const& layoutPath);
+    virtual ~ExampleLayer() = default;
+    
+    bool OnEvent(moth_ui::Event const& event) override;
+    void Update(uint32_t ticks) override;
+    void Draw() override;
+
+    void OnAddedToStack(moth_ui::LayerStack* stack);
+    void OnRemovedFromStack();
+
+    void LoadLayout(std::filesystem::path const& path);
+    std::shared_ptr<moth_ui::Group> GetUIRoot() { return m_root; }
+
+protected:
+    std::shared_ptr<moth_ui::Group> m_root;
+    moth_ui::IntVec2 m_lastDrawnSize;
+
+    bool OnUIEvent(moth_ui::Event const& event);
+    bool OnAnimationStopped(moth_ui::EventAnimationStopped const& event);
+    bool OnRequestQuitEvent(EventRequestQuit const& event);
+};
