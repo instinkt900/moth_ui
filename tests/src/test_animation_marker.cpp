937367b9e36d7c65194f4802443c27e8eb32e684
Matthew Cotton <instinkt@gmail.com>
2026-04-28 14:24:40 +1000

    test: rename animation_event tags to animation_marker

--- tests/src/test_animation_marker.cpp
+++ tests/src/test_animation_marker.cpp
@@ -4,44 +4,44 @@
 
 using namespace moth_ui;
 
-TEST_CASE("AnimationMarker construction", "[animation_event][initialization]") {
+TEST_CASE("AnimationMarker construction", "[animation_marker][initialization]") {
     AnimationMarker ev(5, "hit");
     REQUIRE(ev.frame == 5);
     REQUIRE(ev.name == "hit");
 }
 
-TEST_CASE("AnimationMarker default construction", "[animation_event][initialization]") {
+TEST_CASE("AnimationMarker default construction", "[animation_marker][initialization]") {
     AnimationMarker ev;
     REQUIRE(ev.frame == 0);
     REQUIRE(ev.name.empty());
 }
 
-TEST_CASE("AnimationMarker equality operators", "[animation_event][operators]") {
+TEST_CASE("AnimationMarker equality operators", "[animation_marker][operators]") {
     AnimationMarker a(3, "sound");
     AnimationMarker b(3, "sound");
     REQUIRE(a == b);
     REQUIRE_FALSE(a != b);
 }
 
-TEST_CASE("AnimationMarker inequality on frame", "[animation_event][operators]") {
+TEST_CASE("AnimationMarker inequality on frame", "[animation_marker][operators]") {
     AnimationMarker a(3, "sound");
     AnimationMarker b(4, "sound");
     REQUIRE(a != b);
 }
 
-TEST_CASE("AnimationMarker inequality on name", "[animation_event][operators]") {
+TEST_CASE("AnimationMarker inequality on name", "[animation_marker][operators]") {
     AnimationMarker a(3, "sound");
     AnimationMarker b(3, "effect");
     REQUIRE(a != b);
 }
 
-TEST_CASE("AnimationMarker copy construction", "[animation_event][copy]") {
+TEST_CASE("AnimationMarker copy construction", "[animation_marker][copy]") {
     AnimationMarker original(10, "spawn");
     AnimationMarker copy = original;
     REQUIRE(copy == original);
 }
 
-TEST_CASE("AnimationMarker JSON round-trip", "[animation_event][json]") {
+TEST_CASE("AnimationMarker JSON round-trip", "[animation_marker][json]") {
     AnimationMarker ev(7, "footstep");
     nlohmann::json j;
     to_json(j, ev);
@@ -51,7 +51,7 @@ TEST_CASE("AnimationMarker JSON round-trip", "[animation_event][json]") {
     REQUIRE(restored == ev);
 }
 
-TEST_CASE("AnimationMarker JSON has expected keys", "[animation_event][json]") {
+TEST_CASE("AnimationMarker JSON has expected keys", "[animation_marker][json]") {
     AnimationMarker ev(2, "explosion");
     nlohmann::json j;
     to_json(j, ev);
